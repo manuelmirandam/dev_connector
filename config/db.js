1b05873adf8533fd3aa3dd43fe921a0aeb066663
Manuel Miranda <mmirandasoftware@gmail.com>
2021-01-08 06:11:59 -0700

    Adding mongodb connection

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const moongose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+    try {
+        await moongose.connect(db, { useUnifiedTopology: true, useNewUrlParser: true });
+
+        console.log('MongoDB Connected...');
+    } catch(err) {
+        console.log(err.message);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
